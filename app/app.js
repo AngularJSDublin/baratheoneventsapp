7cf35cb384d2bd8378012b86735bc0cb039ccf6c
PJ <yorko9999@gmail.com>
2015-06-13 14:32:48 +0100

    Added routing

--- app/app.js
+++ app/app.js
@@ -2,6 +2,33 @@
 
 (function(){
 	var app = angular.module('eventsApp', ['ui.router']);
+	
+	app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		
+		$urlRouterProvider.otherwise('/');
+		
+		$stateProvider.state('home', {
+			url: '/',
+			templateUrl: 'views/home.html',
+			controller: 'HomeController'
+		})
+		.state('home.detail', {
+			url: '/',
+			templateUrl: 'views/home.detail.html',
+			controller: 'DetailController'
+		})
+		.state('home.events', {
+			url: '/',
+			templateUrl: 'views/home.events.html',
+			controller: 'EventsController'
+		})
+		.state('home.other', {
+			url: '/',
+			templateUrl: 'views/home.other.html',
+			controller: 'OtherController'
+		});
+		
+	}]);
 
 	//controller as syntax
 	app.controller('SampleController', function(){
