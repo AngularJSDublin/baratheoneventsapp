6c77a154396d0e58a12c8679b4a0c0e4b34ab7e8
PJ <yorko9999@gmail.com>
2015-06-13 14:58:22 +0100

    Moved the routing to its own file

--- app/app.js
+++ app/app.js
@@ -2,41 +2,5 @@
 
 (function(){
 	var app = angular.module('eventsApp', ['ui.router']);
-	
-	app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-		
-		$urlRouterProvider.otherwise('/');
-		
-		$stateProvider.state('home', {
-			url: '/',
-			templateUrl: 'views/home.html',
-			controller: 'HomeController'
-		})
-		.state('home.detail', {
-			url: '/',
-			templateUrl: 'views/home.detail.html',
-			controller: 'DetailController'
-		})
-		.state('home.events', {
-			url: '/',
-			templateUrl: 'views/home.events.html',
-			controller: 'EventsController'
-		})
-		.state('home.other', {
-			url: '/',
-			templateUrl: 'views/home.other.html',
-			controller: 'OtherController'
-		});
-		
-	}]);
-
-	//controller as syntax
-	app.controller('SampleController', function(){
-		this.welcome = 'Welcome to the first AngularJS workshop.'
-	});
-
-	app.controller("SecondController", function($scope){
-		$scope.welcomeAgain = 'By the end of the workshops you will build the Angular events app and hopefully understand the framework on deeper level.'
-	});
 
 })();
