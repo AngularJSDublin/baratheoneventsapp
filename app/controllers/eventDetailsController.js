63bbadd2a948be30314dd6ca4113018f3a92b0f0
wintermuteturbo <wintermuteturbo@gmail.com>
2015-07-04 13:09:00 +0100

    - register modal template - register button triggers modal window

--- app/controllers/eventDetailsController.js
+++ app/controllers/eventDetailsController.js
@@ -1,12 +1,21 @@
 (function() {
     angular.module('eventsApp')
-    .controller('eventDetailsController', ['$scope', '$stateParams', 'eventsService',
-        function($scope, $stateParams, eventsService) {
+    .controller('eventDetailsController', ['$scope', '$stateParams', 'eventsService', '$mdDialog',
+        function($scope, $stateParams, eventsService, $mdDialog) {
         	var eventId = $stateParams.eventId;
         	
         	eventsService.getEvent(eventId).then(function(data) {
         		$scope.eventDetails = data;
         	});
+
+            $scope.registerForEvent = function (ev) {
+                $mdDialog.show({
+                    templateUrl: 'app/views/registerModal.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev
+                })
+            }
         }
+
     ])
 })();
