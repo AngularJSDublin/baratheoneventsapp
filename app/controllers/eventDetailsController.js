b54c3082bb534d4bf70c290bbda742b1c7c1f939
Kzai <killian86@gmail.com>
2015-07-11 15:23:37 +0100

    Added dialogClose and register

--- app/controllers/eventDetailsController.js
+++ app/controllers/eventDetailsController.js
@@ -1,22 +1,66 @@
 (function() {
     angular.module('eventsApp')
-    .controller('eventDetailsController', ['$scope', '$stateParams', 'eventsService', '$mdDialog',
-        function($scope, $stateParams, eventsService, $mdDialog) {
-        	var eventId = $stateParams.eventId;
-        	
-        	eventsService.getEvent(eventId).then(function(data) {
-        		$scope.eventDetails = data;
-        	});
+        .controller('eventDetailsController', ['$scope', '$stateParams', 'eventsService', '$mdDialog',
+            function($scope, $stateParams, eventsService, $mdDialog) {
 
-            $scope.registerForEvent = function (ev) {
-                $mdDialog.show({
-                    controller: "dialogController",
-                    templateUrl: 'app/views/registerModal.html',
-                    parent: angular.element(document.body),
-                
-                })
-            }
-        }
+                var eventId = $stateParams.eventId;
 
-    ])
+                eventsService.getEvent(eventId).then(function (data) {
+                    $scope.eventDetails = data;
+                });
+
+                $scope.registerForEvent = function (eventId) {
+                    $mdDialog.show({
+                        templateUrl: 'app/views/registerModal.html',
+                        parent: angular.element(document.body),
+                        controller: DialogController
+                    });
+
+                    function DialogController($scope, $mdDialog) {
+
+                        $scope.closeDialog = function(){
+                            console.log("Close test");
+                            $mdDialog.hide();
+                        };
+
+                        $scope.submitRegistration = function(){
+
+                            var registrationDetails = {
+
+                                eventId: eventId,
+                                name: $scope.name,
+                                email: $scope.email
+
+                            };
+
+                            console.log(registrationDetails);
+                        }
+                    }
+
+                };
+
+                //Fixme: If navigate backwards, destroy modal. Modal visible if navigate back.
+
+                //$scope.$on('$destroy', function () {
+                //    $mdDialog.hide();
+                //});
+                //
+                //$rootScope.$emit('myEvent');
+                //
+                //$rootScope.$on('myEvent', function (event, data) {
+                //
+                //    if('$destroy'){
+                //        $mdDialog.hide();
+                //    }
+                //
+                //    else {
+                //
+                //
+                //    }
+
+                //})
+
+            }]);
+
+
 })();
